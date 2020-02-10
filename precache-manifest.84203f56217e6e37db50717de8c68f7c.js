ae5f1af07b3e2c19b59f1519b272cdefb9966fbc
Roshini75 <60329908+Roshini75@users.noreply.github.com>
2020-02-10 15:17:22 -0600

    Updates

---
+++ precache-manifest.84203f56217e6e37db50717de8c68f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9650cb1ad1aed96a00cd6385275f9ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6fc94ee2a6e4cba1d99",
+    "url": "/static/css/main.bc9c44a7.chunk.css"
+  },
+  {
+    "revision": "235d39c9d8fb1e5bc231",
+    "url": "/static/js/2.c33fe72d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c33fe72d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6fc94ee2a6e4cba1d99",
+    "url": "/static/js/main.397abaad.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
